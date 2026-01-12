6b627c1c4291d7e61acf3f859ece7b03f2215530
JEStudyhall <jaieelizabeth@gmail.com>
2026-01-12 15:28:10 -0500

    connect to db

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const logger = require("morgan");
+const db = require("./db/connection.js");
 
 const app = express();
 
-app.listen(3000, () => {
+//routes
+app.get("/", (req, res) => {
+  //   res.send("You're on the landing page!!");
+  res.render("index.ejs");
+});
+
+//server that needs to be ran
+db.on("connected", () => {
   console.clear();
-  console.log("Your server is running on PORT 3000");
+  console.log(`Connected to MongoDB!`);
+
+  app.listen(3000, () => {
+    console.log("Your server is running on PORT 3000");
+  });
 });
