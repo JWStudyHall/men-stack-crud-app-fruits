1bd7559cd45b3276181700720169f053a18ccea6
JEStudyhall <jaieelizabeth@gmail.com>
2026-01-12 16:24:56 -0500

    middleware

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const logger = require("morgan");
 const db = require("./db/connection.js");
+const Fruit = require("./models/fruit.js");
 
 const app = express();
+
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
 
 //routes
 app.get("/", (req, res) => {
   //   res.send("You're on the landing page!!");
   res.render("index.ejs");
+});
+
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body)
+  res.redirect("/fruits/new");
 });
 
 //server that needs to be ran
